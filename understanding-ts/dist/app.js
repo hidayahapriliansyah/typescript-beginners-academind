5801f6477f36c973ab3a1457eb36f8ac105680b3
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-14 21:52:28 +0700

    83-intersection-types

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -1,37 +1,10 @@
 "use strict";
-let add;
-add = (n1, n2) => {
-    return n1 + n2;
-};
-// sama kayak ini coy
-// type Person = {
-//     name: string;
-//     age: number;
-//     greet(phrase: string): void;
+// type Admin = {
+//   name: string;
+//   privilege: string[];
 // }
-class Person {
-    constructor(n) {
-        this.age = 30;
-        if (n) {
-            this.name = n;
-        }
-    }
-    greet(phrase) {
-        if (this.name) {
-            console.log(phrase + ' ' + this.name);
-        }
-        else {
-            console.log('Hello!');
-        }
-    }
-}
-let user1;
-// user1 = {
-//   name: 'Hidayah',
-// age: 22,
-//   greet(phrase) {
-//     console.log(phrase + ' ' + this.name);
-//   },
-// };
-user1 = new Person('Adi');
-user1.greet('Hi! I am');
+const employee = {
+    name: 'Hidayah',
+    privilege: ['madang'],
+    startDate: new Date(),
+};
