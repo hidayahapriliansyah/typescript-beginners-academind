92ed0d6cdc1e4fe623aaf52f0947f6104e15823c
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-14 10:23:50 +0700

    create first class

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -1,30 +1,8 @@
 "use strict";
-let userInput;
-let userName;
-userInput = 10;
-userInput = 'Adi';
-if (typeof userInput === 'string') {
-    userName = userInput;
-    console.log(userName);
-}
-//  never type
-function generatorError(message, code) {
-    throw { message: message, code: code };
-}
-;
-// console.log('generatorError(An error occured, 500)');
-// console.log(generatorError('An error occured', 500));
-// ieu ngereunkeun kode dihanapna da throw error nu teu ditangkap
-// jadi si test() teu jalan
-function test() {
-    try {
-        console.log('run test()');
-        generatorError('An error occured', 500);
-    }
-    catch (error) {
-        console.log('error');
-        console.log(error);
+class Departement {
+    constructor(n) {
+        this.name = n;
     }
 }
-;
-test();
+const departement = new Departement('accounting');
+console.log(departement);
--- understanding-ts/src/app.ts
+++ understanding-ts/src/app.ts
@@ -1,32 +1,11 @@
-let userInput: unknown;
-let userName: string;
+class Departement {
+  name: string;
 
-userInput = 10;
-userInput = 'Adi';
-
-if (typeof userInput === 'string') {
-  userName = userInput;
-  console.log(userName);
+  constructor(n: string) {
+    this.name = n;
+  }
 }
 
-//  never type
-function generatorError(message: string, code: number): never {
-  throw { message: message, code: code };
-};
+const departement = new Departement('accounting');
 
-// console.log('generatorError(An error occured, 500)');
-// console.log(generatorError('An error occured', 500));
-// ieu ngereunkeun kode dihanapna da throw error nu teu ditangkap
-// jadi si test() teu jalan
-
-function test() {
-  try {
-    console.log('run test()');
-    generatorError('An error occured', 500)
-  } catch (error) {
-    console.log('error');
-    console.log(error);
-  }
-};
-
-test();
+console.log(departement);
