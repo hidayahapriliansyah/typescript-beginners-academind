80e6770c2048b2c3e742127304211a381c05da12
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-14 13:20:10 +0700

    setter and getter

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -28,17 +28,22 @@ class AccountingDepartmend extends Departement {
     constructor(id, reports) {
         super(id, 'Accounting');
         this.reports = reports;
+        this.lastReport = reports[0];
     }
     addEmployes(employee) {
         if (employee === 'Tolak')
             return;
         this.employees.push(employee);
     }
-    addReport(text) {
+    set addReport(text) {
         this.reports.push('text');
+        this.lastReport = text;
     }
-    getReports() {
-        return this.reports;
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw Error('No report');
     }
 }
 const it = new ITDepartment('d1', ['Sela', 'Dia']);
@@ -56,8 +61,8 @@ accounting.addEmployes('Tolak');
 accounting.addEmployes('Firmansyah');
 accounting.addEmployes('April');
 accounting.printEmployeesInformation();
-accounting.addReport('Report 1');
+accounting.addReport = 'Report 1';
 console.log('accounting with report');
 console.log(accounting);
 console.log('accounting reports');
-console.log(accounting.getReports());
+console.log(accounting.mostRecentReport);
