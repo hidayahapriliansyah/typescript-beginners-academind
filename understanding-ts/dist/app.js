0d487e27f4bdcb26d30658311e69d8e5ce78f8be
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-14 11:01:16 +0700

    61-contruction-function-the-this-keyword

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -3,6 +3,21 @@ class Departement {
     constructor(n) {
         this.name = n;
     }
+    describe() {
+        console.log('Departement : ' + this.name);
+    }
+    // fungsi this: Departement
+    // nanti kalau ada object yang pake describe dari instance ini
+    //  maka si struktur objeknya harus mirip dengan class ini.
+    call() {
+        console.log('call' + this.name);
+    }
 }
-const departement = new Departement('accounting');
-console.log(departement);
+const accounting = new Departement('Accounting');
+accounting.describe();
+const accountingCopy = { name: 'Hello', describe: accounting.describe, call: accounting.call };
+accountingCopy.describe();
+accountingCopy.call();
+const accountingCopyNoDiscribe = { call: accounting.call };
+console.log('accountingCopyNoDiscribe.call()');
+accountingCopyNoDiscribe.call();
