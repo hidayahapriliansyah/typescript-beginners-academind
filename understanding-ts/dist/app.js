8ac3569a68dcffb7791ac1cf053c2ece02d39301
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-16 21:20:51 +0700

    97-another-generic-function

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -6,3 +6,15 @@ function mergeObj(objA, objB) {
 ;
 const merge = mergeObj({ hello: 'hello' }, { age: 22 });
 console.log(merge.hello);
+function countAndDescribe(element) {
+    let description = 'Got no element';
+    if (element.length === 1) {
+        description = 'Got 1 element';
+    }
+    else if (element.length > 1) {
+        description = 'Got ' + element.length + ' element';
+    }
+    return [element, description];
+}
+;
+console.log(countAndDescribe('Adi'));
--- understanding-ts/src/app.ts
+++ understanding-ts/src/app.ts
@@ -20,3 +20,19 @@ function mergeObj<T extends Hello, U extends object>(objA: T, objB: U) {
 
 const merge = mergeObj({ hello: 'hello' }, { age: 22 });
 console.log(merge.hello);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) : [T, string]  {
+  let description = 'Got no element';
+  if (element.length === 1) {
+    description = 'Got 1 element';
+  } else if (element.length > 1) {
+    description = 'Got ' + element.length + ' element';
+  }
+  return [element, description];
+};
+
+console.log(countAndDescribe('Adi'));
