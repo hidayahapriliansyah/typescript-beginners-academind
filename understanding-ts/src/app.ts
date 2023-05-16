5dee4237f7f8cb4dd5cc5dd73101e346d1f8764d
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-16 19:46:32 +0700

    94-built-in-and-why-generic

--- understanding-ts/src/app.ts
+++ understanding-ts/src/app.ts
@@ -1,33 +1,11 @@
-type Combinable = number | string;
+const names: Array<string> = [];
 
-function add(a: number, b: number): number;
-function add(a: string, b: number): string;
-function add(a: number, b: string): string;
-function add(a: string, b: string): string;
-function add(a: Combinable, b: Combinable): Combinable {
-  if (typeof a === 'string' || typeof b === 'string') {
-    return a.toLocaleString() + b.toLocaleString();
-  }
-  return a + b;
-};
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('It is done!');
+  }, 2000);
+});
 
-const result = add(2, 'Hello');
-result.split(' ');
-
-const fetchData = {
-  id: 'u1',
-  name: 'Hidayaha',
-  job: {
-    title: 'Front End Programmer',
-    description: 'My own componay',
-  }
-};
-
-console.log(fetchData?.job?.title);
-
-const userInput = '';
-//  '' dianggap false kalau pakai ||
-
-const nameInput = userInput ?? 'DEFAULT';
-
-console.log('name', nameInput);
+promise.then((response) => {
+  response.toLocaleLowerCase();
+});
