0dc8d028255e1e8bbc60090d8dde4172ea9dec0d
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-16 12:35:39 +0700

    85-discriminated-union

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -1,57 +1,27 @@
 "use strict";
-// type Admin = {
-//   name: string;
-//   privilege: string[];
-// }
-const employee = {
-    name: 'Hidayah',
-    privilege: ['madang'],
-    startDate: new Date(),
-};
-function add(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toLocaleString() + b.toLocaleString();
+function movingAnimal(animal) {
+    // kenapa ada Discrimenated Union?
+    // karena gak bisa kayak gini
+    // if ('flyingSpeed' in animal) {
+    //   console.log('Moving with speed ' + animal.flyingSpeed);
+    // }
+    // if ('flyingSpeed' instanceof Bird) {
+    //   console.log('Moving with speed ' + animal.flyingSpeed);
+    // }
+    // bukan karena bakal muncul banyak if if if nya ...
+    //  tapi karena kalau kita cek pake in
+    //  karena in itu buat objek, bukan interface,
+    // sama kayak instance itu buat ngecek apakah objek instance dari class,
+    // sedangkan kita bekerja dengan interface sekarang ...
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
     }
-    return a + b;
+    console.log('Moving at speed : ' + speed);
 }
-;
-function printEmployeeInformation(emp) {
-    if ('privileges' in emp) {
-        console.log('Privileges admin : ' + emp.privileges);
-    }
-    if ('startDate' in emp) {
-        console.log('Startdate Employee : ' + emp.startDate);
-    }
-}
-;
-const emp1 = {
-    name: 'Adi',
-    startDate: new Date(),
-    privilege: ['apa'],
-};
-printEmployeeInformation(emp1);
-// pakai instance of
-class Car {
-    drive() {
-        console.log('Drive ...');
-    }
-}
-class Truck {
-    drive() {
-        console.log('Drive ...');
-    }
-    ;
-    loadCargo(amount) {
-        console.log('Loading cargo ...' + amount);
-    }
-    ;
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-    vehicle.drive();
-}
-useVehicle(v2);
+movingAnimal({ type: 'bird', flyingSpeed: 50 });
