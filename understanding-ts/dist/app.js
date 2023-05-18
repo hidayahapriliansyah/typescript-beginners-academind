8c83fe4d55484206ccb9747fed9fb67b737e0583
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-18 11:02:03 +0700

    106-working-with-decorators-vactories

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -1,37 +1,26 @@
 "use strict";
-// function Logger(hahaha: Function) {
-//   console.log('Logging ...');
-//   console.log(hahaha);
-// };
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-// class Person {
-//   name = 'Hidayah';
-//   constructor() {
-//     console.log('Creating person object ...');
-//   };
-//   @Logger('sdfsdf')
-//   wiwLogger() {
-//     console.log('Wiw loger');
-//   }
-// }
-// const person = new Person();
-// console.log(person);
-// 
-function logMethod(target, key, descriptor) {
-    // Modifikasi perilaku metode di sini
-    console.log(`Metode ${key} telah didekorasi`);
+function Logger(logString) {
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
 }
-class Example {
-    greet() {
-        console.log('Halo!');
+;
+let Person = class Person {
+    constructor() {
+        this.name = 'Hidayah';
+        console.log('Creating person object ...');
     }
-}
-__decorate([
-    logMethod
-], Example.prototype, "greet", null);
-const examplee = new Example();
+    ;
+};
+Person = __decorate([
+    Logger('LOGGING PERSON')
+], Person);
+const person = new Person();
+console.log(person);
--- understanding-ts/src/app.ts
+++ understanding-ts/src/app.ts
@@ -1,36 +1,19 @@
-// function Logger(hahaha: Function) {
-//   console.log('Logging ...');
-//   console.log(hahaha);
-// };
-
-// class Person {
-//   name = 'Hidayah';
-  
-//   constructor() {
-//     console.log('Creating person object ...');
-//   };
-  
-//   @Logger('sdfsdf')
-//   wiwLogger() {
-//     console.log('Wiw loger');
-//   }
-// }
-
-// const person = new Person();
-
-// console.log(person);
-
-// 
-function logMethod(target: any, key: string, descriptor: PropertyDescriptor) {
-  // Modifikasi perilaku metode di sini
-  console.log(`Metode ${key} telah didekorasi`);
-}
-
-class Example {
-  @logMethod
-  greet() {
-    console.log('Halo!');
+function Logger(logString: string) {
+  return function(constructor:  Function) {
+    console.log(logString);
+    console.log(constructor);
   }
+};
+
+@Logger('LOGGING PERSON')
+class Person {
+  name = 'Hidayah';
+  
+  constructor() {
+    console.log('Creating person object ...');
+  };
 }
 
-const examplee = new Example();
+const person = new Person();
+
+console.log(person);
