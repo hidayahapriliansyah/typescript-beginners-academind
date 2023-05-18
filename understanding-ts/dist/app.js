d998e36df1df86d0be897268164d2f81b413f04f
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-18 13:37:04 +0700

    107-building-more-useful-decorator

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -12,15 +12,42 @@ function Logger(logString) {
     };
 }
 ;
-let Person = class Person {
+function WithTemplate(template, hookId) {
+    return function (constructor) {
+        const hookEl = document.getElementById('app');
+        console.log('typeof constructor');
+        console.log(typeof constructor);
+        console.log(constructor);
+        const p = new constructor();
+        console.log('typeof p');
+        console.log(typeof p);
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            console.log(p, 'peeeeeeeeee');
+            hookEl.querySelector('h1').textContent = p.name;
+        }
+    };
+}
+;
+// function testConstructor(constructor: Function | any) {
+//   console.log('dari mana nich');
+//   console.log(constructor);
+//   const p = new constructor();
+//   console.log(p.name);
+//   console.log('testConstructor terpanggil');
+// }
+// @Logger('LOGGING PERSON')
+let Person = 
+// @testConstructor
+class Person {
     constructor() {
-        this.name = 'Hidayah';
-        console.log('Creating person object ...');
+        this.name = 'Adi';
+        console.log('Heloo');
     }
-    ;
 };
 Person = __decorate([
-    Logger('LOGGING PERSON')
+    WithTemplate('<h1>My person Object</h1>', 'app')
+    // @testConstructor
 ], Person);
 const person = new Person();
-console.log(person);
+console.log(Person);
--- understanding-ts/src/app.ts
+++ understanding-ts/src/app.ts
@@ -1,19 +1,46 @@
 function Logger(logString: string) {
-  return function(constructor:  Function) {
+  return function(constructor: Function) {
     console.log(logString);
     console.log(constructor);
   }
 };
 
-@Logger('LOGGING PERSON')
+function WithTemplate(template: string, hookId: string) {
+  return function(constructor: any) {
+    const hookEl = document.getElementById('app')!;
+    console.log('typeof constructor');
+    console.log(typeof constructor);
+    console.log(constructor);
+    const p = new constructor();
+    console.log('typeof p');
+    console.log(typeof p);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      console.log(p, 'peeeeeeeeee');
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  }
+};
+
+// function testConstructor(constructor: Function | any) {
+//   console.log('dari mana nich');
+//   console.log(constructor);
+//   const p = new constructor();
+//   console.log(p.name);
+//   console.log('testConstructor terpanggil');
+// }
+
+// @Logger('LOGGING PERSON')
+@WithTemplate('<h1>My person Object</h1>', 'app')
+// @testConstructor
 class Person {
-  name = 'Hidayah';
-  
+  name = 'Adi';
+
   constructor() {
-    console.log('Creating person object ...');
-  };
+    console.log('Heloo');
+  }
 }
 
 const person = new Person();
+console.log(Person);
 
-console.log(person);
