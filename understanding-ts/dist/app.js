28d3b2e36d94bd35cb1f934cbf12f827d46a7556
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-18 14:31:14 +0700

    109-diving-into-property-decorators

--- understanding-ts/dist/app.js
+++ understanding-ts/dist/app.js
@@ -14,33 +14,33 @@ function Logger(logString) {
     };
 }
 ;
-function WithTemplate(template, hookId) {
-    console.log('TEMPLATE DECORATOR');
-    return function (constructor) {
-        console.log('TEMPLATE TRUE DECORATOR');
-        const hookEl = document.getElementById('app');
-        console.log('typeof constructor');
-        console.log(typeof constructor);
-        console.log(constructor);
-        const p = new constructor();
-        console.log('typeof p');
-        console.log(typeof p);
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            console.log(p, 'peeeeeeeeee');
-            hookEl.querySelector('h1').textContent = p.name;
-        }
-    };
-}
-;
-// function testConstructor(constructor: Function | any) {
-//   console.log('dari mana nich');
+// function WithTemplate(template: string, hookId: string) {
+//   console.log('TEMPLATE DECORATOR');
+// return function(constructor: any) {
+//   console.log('TEMPLATE TRUE DECORATOR');
+//   const hookEl = document.getElementById('app')!;
+//   console.log('typeof constructor');
+//   console.log(typeof constructor);
 //   console.log(constructor);
 //   const p = new constructor();
-//   console.log(p.name);
-//   console.log('testConstructor terpanggil');
+//   console.log('typeof p');
+//   console.log(typeof p);
+//   if (hookEl) {
+//     hookEl.innerHTML = template;
+//     console.log(p, 'peeeeeeeeee');
+//     hookEl.querySelector('h1')!.textContent = p.name;
+//   }
 // }
+// };
+// // function testConstructor(constructor: Function | any) {
+// //   console.log('dari mana nich');
+// //   console.log(constructor);
+// //   const p = new constructor();
+// //   console.log(p.name);
+// //   console.log('testConstructor terpanggil');
+// // }
 let Person = 
+// @WithTemplate('<h1>My person Object</h1>', 'app')
 // @testConstructor
 class Person {
     constructor() {
@@ -49,9 +49,45 @@ class Person {
     }
 };
 Person = __decorate([
-    Logger('LOGGING PERSON'),
-    WithTemplate('<h1>My person Object</h1>', 'app')
+    Logger('LOGGING PERSON')
+    // @WithTemplate('<h1>My person Object</h1>', 'app')
     // @testConstructor
 ], Person);
 const person = new Person();
-console.log(Person);
+const person2 = new Person();
+// console.log(Person);
+// property decorator
+// function Log(target: any, propertyName: string | Symbol) {
+//   console.log('Property decorator!');
+//   console.log('target');
+//   console.log(target);
+//   console.log('propertyName');
+//   console.log(propertyName);
+// }
+// class Product {
+//   @Log
+//   title: string;
+//   private _price: number;
+//   constructor(t: string, p: number) {
+//     this.title = t;
+//     this._price = p;
+//   }
+//   set price(val: number) {
+//     if (val < 0) {
+//       throw new Error('Price must be not 0');
+//     }
+//     this._price = val;
+//   }
+//   getPriceWithTax(tax: number) {
+//     return this._price * (1 + tax);
+//   }
+// }
+// function CheckDecorate (target: Function) {
+//   console.log('jalaaan ....');
+// };
+// @CheckDecorate
+// class CheckClass {
+//   constructor() {
+//     console.log('Constructo checkClass');
+//   }
+// }
--- understanding-ts/src/app.ts
+++ understanding-ts/src/app.ts
@@ -7,35 +7,35 @@ function Logger(logString: string) {
   }
 };
 
-function WithTemplate(template: string, hookId: string) {
-  console.log('TEMPLATE DECORATOR');
-  return function(constructor: any) {
-    console.log('TEMPLATE TRUE DECORATOR');
-    const hookEl = document.getElementById('app')!;
-    console.log('typeof constructor');
-    console.log(typeof constructor);
-    console.log(constructor);
-    const p = new constructor();
-    console.log('typeof p');
-    console.log(typeof p);
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      console.log(p, 'peeeeeeeeee');
-      hookEl.querySelector('h1')!.textContent = p.name;
-    }
-  }
-};
+// function WithTemplate(template: string, hookId: string) {
+//   console.log('TEMPLATE DECORATOR');
+  // return function(constructor: any) {
+  //   console.log('TEMPLATE TRUE DECORATOR');
+  //   const hookEl = document.getElementById('app')!;
+  //   console.log('typeof constructor');
+  //   console.log(typeof constructor);
+  //   console.log(constructor);
+  //   const p = new constructor();
+  //   console.log('typeof p');
+  //   console.log(typeof p);
+  //   if (hookEl) {
+  //     hookEl.innerHTML = template;
+  //     console.log(p, 'peeeeeeeeee');
+  //     hookEl.querySelector('h1')!.textContent = p.name;
+  //   }
+  // }
+// };
 
-// function testConstructor(constructor: Function | any) {
-//   console.log('dari mana nich');
-//   console.log(constructor);
-//   const p = new constructor();
-//   console.log(p.name);
-//   console.log('testConstructor terpanggil');
-// }
+// // function testConstructor(constructor: Function | any) {
+// //   console.log('dari mana nich');
+// //   console.log(constructor);
+// //   const p = new constructor();
+// //   console.log(p.name);
+// //   console.log('testConstructor terpanggil');
+// // }
 
 @Logger('LOGGING PERSON')
-@WithTemplate('<h1>My person Object</h1>', 'app')
+// @WithTemplate('<h1>My person Object</h1>', 'app')
 // @testConstructor
 class Person {
   name = 'Adi';
@@ -46,5 +46,49 @@ class Person {
 }
 
 const person = new Person();
-console.log(Person);
+const person2 = new Person();
+// console.log(Person);
 
+// property decorator
+
+// function Log(target: any, propertyName: string | Symbol) {
+//   console.log('Property decorator!');
+//   console.log('target');
+//   console.log(target);
+//   console.log('propertyName');
+//   console.log(propertyName);
+// }
+
+// class Product {
+//   @Log
+//   title: string;
+//   private _price: number;
+
+//   constructor(t: string, p: number) {
+//     this.title = t;
+//     this._price = p;
+//   }
+
+//   set price(val: number) {
+//     if (val < 0) {
+//       throw new Error('Price must be not 0');
+//     }
+
+//     this._price = val;
+//   }
+
+//   getPriceWithTax(tax: number) {
+//     return this._price * (1 + tax);
+//   }
+// }
+
+// function CheckDecorate (target: Function) {
+//   console.log('jalaaan ....');
+// };
+
+// @CheckDecorate
+// class CheckClass {
+//   constructor() {
+//     console.log('Constructo checkClass');
+//   }
+// }
