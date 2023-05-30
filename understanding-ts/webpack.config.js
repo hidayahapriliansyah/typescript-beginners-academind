0c2a92a4e36f35f81c74f41a8a7d6335759c6767
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-30 21:40:59 +0700

    157-finishing-the-setup-adn-adding-web-dev-server

--- understanding-ts/webpack.config.js
+++ understanding-ts/webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.ts',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
   },
   devtool: 'inline-source-map',
   module: {
@@ -12,11 +21,11 @@ module.exports = {
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.js']
-  }
+    extensions: ['.ts', '.js'],
+  },
 };
