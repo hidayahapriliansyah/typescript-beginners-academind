be43f7e1c36d7bbb62cb7f835e5adf17dada1860
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-05-31 14:16:29 +0700

    180-getting-user-input-wit-ref

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 function App() {
   const todos = [{ id: 't1', text: 'Finish the course'}];
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       {/* components add */}
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
---
+++ react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react';
+
+const NewTodo: React.FC = () => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  console.log('textInputRef before render');
+  console.log(textInputRef);
+  
+  const todoSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    // textInputRef.current!.focus();
+    console.log('textInputRef after render');
+    console.log(textInputRef);
+    console.log(enteredText);
+  };
+
+  return (
+    <form onSubmit={todoSubmitHandler}>
+      <div>
+        <label htmlFor='todo-text'>Todo Text</label>
+        <input type='text' id='todo-text' ref={textInputRef}/>
+      </div>
+      <button type='submit'>ADD TODO</button>
+    </form>
+  );
+};
+
+export default NewTodo;
